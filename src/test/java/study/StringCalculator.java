80cc1a94bcd3c0188fc1cabb844fc708d9253f6a
sanghaklee <leesanghak7298@gmail.com>
2021-08-10 19:44:31 +0900

    draft: step1

---
+++ src/test/java/study/StringCalculator.java
@@ -0,0 +1,9 @@
+package study;
+
+public class StringCalculator {
+    public int add(String text) {
+        if (text == null) return 0;
+        if (text.isEmpty()) return 0;
+        return 0;
+    }
+}
